d6ad874fc2015c1d2c46cbfb7458e780d310c84f
Administrator <Administrator@501-S26>
2022-04-26 10:08:59 +0900

    0426

--- 0425/src/TriangleExam.java
+++ 0425/src/TriangleExam.java
@@ -1,27 +1,24 @@
-
 public class TriangleExam {
-
-	public static void main(String[] args) {
-		Triangle tr = new Triangle(10.2 , 17.3);
-		System.out.println("»ï°¢ÇüÀÇ ³ÐÀÌ: "+ tr.getArea());
-		tr.setSize(7.5, 9.2);
-		System.out.println("º¯°æµÈ »ï°¢ÇüÀÇ ³ÐÀÌ: "+ tr.getArea());
-
+	public static void main(String args[]) {
+		 Triangle tr = new Triangle(10.2, 17.3);
+		 System.out.println("»ï°¢ÇüÀÇ ³ÐÀÌ: " + tr.getSize());
+		 tr.setSize(7.5, 9.2);
+		 System.out.println("º¯°æµÈ »ï°¢ÇüÀÇ ³ÐÀÌ: " + tr.getSize());
 	}
-
 }
+
 class Triangle{
 	double bottom;
 	double height;
 	public Triangle(double bt, double hg) {
-		this.bottom = bt;
-		this.height = hg;
+		 this.bottom = bt;
+		 this.height = hg;
 	}
 	public void setSize(double bt, double hg) {
-		this.bottom = bt;
-		this.height = hg;
-	}
-	public double getArea() {
+		 this.bottom = bt;
+		 this.height = hg;
+	 }
+	public double getSize() {
 		return bottom*height/2;
 	}
 }
